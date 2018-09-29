ad3a1d545527eaa914580ee98f4814556ced2952
Frank Fang <frankfang2014@hotmail.com>
2018-09-29 13:00:30 -0700

    fixed 404 after refreshing the page

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {HashRouter,Route} from 'react-router-dom';
 import Tspace from './projectdetail/tspace';
 import ChinaPJ from './projectdetail/countrypj';
 import Weather from './projectdetail/weather';
 import MyPortfolio from './projectdetail/portfolio';
 
 ReactDOM.render(
-<BrowserRouter>
+<HashRouter>
     <div>
     <Route  exact path="/" component={App}/>
     <Route  path="/tspacedetail" component={Tspace}/>
@@ -17,6 +17,6 @@ ReactDOM.render(
     <Route  path="/weatherdetail" component={Weather}/>
     <Route  path="/myportfoliodetail" component={MyPortfolio}/>
     </div>
-</BrowserRouter>
+</HashRouter>
 , document.getElementById('root'));
 
