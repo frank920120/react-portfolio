c6c7e4eb4aa03de70ad1538942a906a672cf84ec
Frank Fang <frankfang2014@hotmail.com>
2019-06-06 14:27:44 -0700

    fix footer height

--- react-portfolio/src/common/footer/style.js
+++ react-portfolio/src/common/footer/style.js
@@ -1,95 +1,76 @@
-import styled from 'styled-components';
-import '../../style';
+import styled from "styled-components";
+import "../../style";
 
 export const FooterWrapper = styled.footer`
-  font-family:'Poppins','Helvetica',sans-serif;
-background:#151515;
-height:350px;
-width:100%;
-display:flex;
-justify-content:space-around;
-position:relative;
-@media (max-width:600px){
-  flex-direction:column;
-  align-items:center;
-  text-align:center;
-  position:relative;
-}
-`
-
+  font-family: "Poppins", "Helvetica", sans-serif;
+  background: #151515;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  position: relative;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    position: relative;
+  }
+`;
 
 export const ContactWrapper = styled.div`
-
-display:flex;
-flex-direction:column;
-justify-content:space-evenly;
-padding:20px;
-
-
-`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 20px;
+`;
 
 export const ContactInfo = styled.h3`
-
-margin:0;
-color:#fff;
-font-size:20px;
-line-height:70px;
-&.ContactHeader{
-    color:#ed6464;
-    font-size:30px;
-}
-
-`
+  margin: 0;
+  color: #fff;
+  font-size: 20px;
+  line-height: 70px;
+  &.ContactHeader {
+    color: #ed6464;
+    font-size: 30px;
+  }
+`;
 
 export const SocialMediaWrapper = styled.div`
-
-width:200px;
-display:flex;
-justify-content:space-around;
-align-items:center;
-@media (max-width:600px){
-  padding:10px;
-        
-}
-
-`
+  width: 200px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  @media (max-width: 600px) {
+    padding: 10px;
+  }
+`;
 export const SocialMediaItemLink = styled.a`
-display:block;
-
-`
+  display: block;
+`;
 export const SocialMediaItem = styled.img`
-width:30px;
-height:30px;
-display:block;
+  width: 30px;
+  height: 30px;
+  display: block;
 
-&:hover{
+  &:hover {
+    color: white;
+  }
+`;
 
-color:white;
-
-
-}
-
-`
-
-export const TextSpan =styled.span`
-
-font-size:15px;
-font-weight:400;
-
-`
+export const TextSpan = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+`;
 
 export const Copyright = styled.h3`
-position:absolute;
-transform:translate(-50%,-50%);
-bottom:0;
-left:50%;
-font-size:10px;
-text-align:center;
-color:white;
-@media (max-width:600px){
-  width:100%;
- position:relative;
- 
-}
-`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  bottom: 0;
+  left: 50%;
+  font-size: 10px;
+  text-align: center;
+  color: white;
+  @media (max-width: 600px) {
+    width: 100%;
+    position: relative;
+  }
+`;
